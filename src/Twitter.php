d344a0fdda38f016896e8e4c5d2805ec500d2d2d
Igor Dozmorov <dozmorov@kassy.ru>
2020-05-25 01:32:31 +0500

    init

---
+++ src/Twitter.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Dozmorov\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Twitter
+ *
+ */
+class Twitter extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    public $domain = 'https://twitter.com';
+    public $apiDomain = 'https://api.twitter.com/';
+
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->apiDomain . 'oauth2/token';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param array $params
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->apiDomain . 'oauth2/token';
+    }
+
+    /**
+     * Get provider url to fetch user details.
+     *
+     * NOTE:
+     * By default twitter does not allow to retrieve email address.
+     * To get access to user email application need to be whitelisted by twitter.
+     *
+     * @param AccessToken $token
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return $this->apiDomain . 'account/verify_credentials';
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400)
+        {
+            $message = '';
+            if (!empty($data['errors']) && is_array($data['errors']))
+            {
+                $error = array_shift($data['errors']);
+                if (!empty($error['code']))
+                {
+                    $message .= $error['code'];
+                }
+                if (!empty($error['code']) && !empty($error['message']))
+                {
+                    $message .= ': ';
+                }
+                if (!empty($error['message']))
+                {
+                    $message .= $error['message'];
+                }
+            }
+            throw new IdentityProviderException(
+                $message ? : $response->getReasonPhrase(), $response->getStatusCode(), $response
+            );
+        }
+    }
+
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return (new TwitterResourceOwner($response))->setDomain($this->domain);
+    }
+
+}
---
+++ src/TwitterResourceOwner.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Dozmorov\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+
+/**
+ * TwitterUser
+ *
+ */
+class TwitterResourceOwner implements ResourceOwnerInterface
+{
+    /**
+     * Domain
+     *
+     * @var string
+     */
+    protected $domain;
+
+    /**
+     * Raw response
+     *
+     * @var array
+     */
+    protected $response;
+
+    /**
+     * @param  mixed[] $response
+     */
+    public function __construct(array $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get resource owner id
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        return $this->response['id'] ? : null;
+    }
+
+    /**
+     * Get resource owner email
+     *
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->response['email'] ? : null;
+    }
+
+    /**
+     * Get resource owner name
+     *
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->response['name'] ? : null;
+    }
+
+    /**
+     * Get resource owner nickname
+     *
+     * @return string|null
+     */
+    public function getNickname()
+    {
+        return $this->response['screen_name'] ? : null;
+    }
+
+    /**
+     * Get resource owner url
+     *
+     * @return string|null
+     */
+    public function getUrl()
+    {
+        return trim($this->domain . '/' . $this->getNickname()) ? : null;
+    }
+
+    /**
+     * Set resource owner domain
+     *
+     * @param  string $domain
+     *
+     * @return TwitterResourceOwner
+     */
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
+
+}
